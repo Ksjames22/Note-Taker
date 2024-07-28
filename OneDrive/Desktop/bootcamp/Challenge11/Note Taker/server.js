26c89ca178b4b7ca65e62cfe7e8fc934a0757ca8
Kalen <167487804+Ksjames22@users.noreply.github.com>
2024-07-27 23:46:57 -0400

    updates

--- OneDrive/Desktop/bootcamp/Challenge11/Note Taker/server.js
+++ OneDrive/Desktop/bootcamp/Challenge11/Note Taker/server.js
@@ -1,17 +1,26 @@
+// Dependencies
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
 
+// app use express
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+// creating environment variable port
+const PORT = process.env.PORT || 3001;
 
+
+// asks express to create a route for every file in the 'public' folder and give it a '/' route
+app.use(express.static('public'));
+// sets up express app to handel data parser, middle wear created req.body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
 
+
+// routes to route files
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
+
+
+// app listener - starts the server
 app.listen(PORT, () => {
-    console.log(`Now listening on PORT: ${PORT}`);
+  console.log(`Server available at localhost${PORT}`);
 });
