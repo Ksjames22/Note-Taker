ffcbc49012e57344c49cfa2e16bba823250d82dc
Ksjames22 <167487804+Ksjames22@users.noreply.github.com>
2024-07-30 20:27:19 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+// Dependencies
+const express = require('express');
+
+// app use express
+const app = express();
+
+// creating environment variable port
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server available at http://localhost:${PORT}`);
+});
+
+
+// asks express to create a route for every file in the 'public' folder and give it a '/' route
+app.use(express.static('public'));
+// sets up express app to handel data parser, middle wear created req.body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// routes to route files
+require('./routes/apiroutes')(app);
+require('./routes/htmlroutes')(app);
+
+
+// app listener - starts the server
+app.listen(PORT, () => {
+  console.log(`Server available at localhost${PORT}`);
+});
